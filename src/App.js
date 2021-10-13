66d9018dc50519af2d36a4457cde1363f539fe2a
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2021-10-13 18:17:27 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Form from 'components/Form/Form';
+import Button from 'components/TableMatrix/Button/Button';
+import TableHeader from 'components/TableMatrix/TableHeader/TableHeader';
+
+function App() {
+  return (
+    <>
+      <Form />
+      <section className={styles.sectionTable}>
+        <Button />
+        <TableHeader columns={5} />
+      </section>
+    </>
+  );
+}
+
+export default App;
